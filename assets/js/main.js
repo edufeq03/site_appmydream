f6c430bcaa96106bb0eec41fd5b55c2555925019
Eduardo Capella <edufeq03@gmail.com>
2026-01-17 18:35:20 -0300

    feat: inclusao das paginas

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// JS propositalmente simples
+// Aqui é onde você pode evoluir depois (menu mobile, animações, etc.)
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("AppMyDream site carregado com sucesso");
+});
